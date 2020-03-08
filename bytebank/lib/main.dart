583b8513541a3e4276ebec0f65110784fdb2af16
raphapfeifer <rapha_pfeifer@yahoo.com.br>
2020-03-08 18:22:51 -0300

    Primeiro Commit

---
+++ bytebank/lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(BytebankApp());
+
+class BytebankApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primaryColor: Colors.green[900],
+        accentColor: Colors.blueAccent[700],
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('DashBoard'),
+        ),
+      ),
+    );
+  }
+}
