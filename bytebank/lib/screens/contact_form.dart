c4c1557677b99cc2a2f61e3902556bb19b49e59b
raphapfeifer <rapha_pfeifer@yahoo.com.br>
2020-03-12 22:07:04 -0300

    Implementanto tela de formulário

---
+++ bytebank/lib/screens/contact_form.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+const _tituloAppBar = 'New Contact';
+
+const _labelFullName = 'Full name';
+
+const _labelAccountNumber = 'Account Number';
+
+const _createButton = 'Create';
+
+class ContactForm extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(_tituloAppBar),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: <Widget>[
+            TextField(
+              decoration: InputDecoration(labelText: _labelFullName),
+              style: TextStyle(
+                fontSize: 24.0,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 8.0),
+              child: TextField(
+                decoration: InputDecoration(labelText: _labelAccountNumber),
+                style: TextStyle(
+                  fontSize: 24.0,
+                ),
+                keyboardType: TextInputType.number,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 16.0),
+              child: SizedBox(
+                width: double.maxFinite,
+                child: RaisedButton(
+                  child: Text(_createButton),
+                  onPressed: () {},
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
