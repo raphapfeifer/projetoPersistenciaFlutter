c62879e8a57ff915761ab0f78c5fa94806de1452
raphapfeifer <rapha_pfeifer@yahoo.com.br>
2020-03-22 13:27:55 -0300

    Organização do arquivo constants

--- bytebank/lib/resources/constants.dart
+++ bytebank/lib/resources/constants.dart
@@ -1,11 +1,14 @@
 library constants;
 
+//Titles
 const String TITULO_DASHBOARD = 'DashBoard';
-const String TITULO_TRANSFER = 'Transfer';
 const String TITULO_TRANSACTION_FEED = 'Transaction Feed';
-
-// Tela Form
+const String TITULO_TRANSFER = 'Transfer';
 const String TITULO_NEW_CONTACT = 'New Contact';
+
+//Labels
 const String LABEL_FULL_NAME = 'Full name';
 const String LABEL_ACCOUNT_NUMBER = 'Account Number';
+
+//Buttons
 const String CREATE_BUTTON = 'Create';
