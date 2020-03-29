73d2b7b8234a23ceb2801a0a32cf2e668ff0cc75
raphapfeifer <rapha_pfeifer@yahoo.com.br>
2020-03-29 01:00:08 -0300

    Inclusão de interceptors

--- bytebank/lib/http/webclient.dart
+++ bytebank/lib/http/webclient.dart
@@ -1,7 +1,32 @@
 
 
 import 'package:http/http.dart';
+import 'package:http_interceptor/http_interceptor.dart';
+
+
+class LoggingInterceptor implements InterceptorContract {
+  @override
+  Future<RequestData> interceptRequest({RequestData data}) async {
+    print('Request');
+    print('url: ${data.baseUrl}');
+    print('headers: ${data.headers}');
+    print('body: ${data.body}');
+    return data;
+  }
+
+  @override
+  Future<ResponseData> interceptResponse({ResponseData data}) async {
+    print('Response');
+    print('status code: ${data.statusCode}');
+    print('headers: ${data.headers}');
+    print('body: ${data.body}');
+    return data;
+  }
+
+}
+
 
 void findAll() async {
-  final Response response = await get("http://192.168.15.33:8080/transactions");
+  final Client client = HttpClientWithInterceptor.build(interceptors: [LoggingInterceptor()]);
+  final Response response = await client.get("http://192.168.15.33:8080/transactions");
 }
