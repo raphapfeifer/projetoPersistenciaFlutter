f5244bea3db88865bac74b4f3e9e37562edd5f61
raphapfeifer <rapha_pfeifer@yahoo.com.br>
2020-03-31 23:53:54 -0300

    Implementação de método que busca dados da API

--- bytebank/lib/screens/transactions_list.dart
+++ bytebank/lib/screens/transactions_list.dart
@@ -1,4 +1,6 @@
 
+import 'package:bytebank/components/progress.dart';
+import 'package:bytebank/http/webclient.dart';
 import 'package:bytebank/models/Transaction.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -6,7 +8,6 @@ import 'package:bytebank/resources/constants.dart' as Constants;
 
 class TransactionsList extends StatelessWidget {
 
-  final List<Transaction> transactions = List();
 
   @override
   Widget build(BuildContext context) {
@@ -14,29 +15,49 @@ class TransactionsList extends StatelessWidget {
       appBar: AppBar(
         title: Text(Constants.TITULO_TRANSACTION_FEED),
       ),
-      body: ListView.builder(
-          itemBuilder:(context,index){
-            final Transaction transaction = transactions[index];
-            return Card(
-                child: ListTile(
-                  leading: Icon(Icons.monetization_on),
-                  title: Text(
-                    transaction.value.toString(),
-                    style: TextStyle(
-                      fontSize: 24.0,
-                      fontWeight: FontWeight.bold
+      body: FutureBuilder<List<Transaction>>(
+        future: findAll(),
+        builder:(context,snapshot){
+
+          switch(snapshot.connectionState){
+
+            case ConnectionState.none:
+              break;
+            case ConnectionState.waiting:
+              return Progress();
+              break;
+            case ConnectionState.active:
+              break;
+            case ConnectionState.done:
+              final List<Transaction> transactions = snapshot.data;
+              return  ListView.builder(
+                itemBuilder:(context,index){
+                  final Transaction transaction = transactions[index];
+                  return Card(
+                    child: ListTile(
+                      leading: Icon(Icons.monetization_on),
+                      title: Text(
+                        transaction.value.toString(),
+                        style: TextStyle(
+                            fontSize: 24.0,
+                            fontWeight: FontWeight.bold
+                        ),
+                      ),
+                      subtitle: Text(
+                        transaction.contact.accountNumber.toString(),
+                        style: TextStyle(
+                          fontSize: 16.0,
+                        ),
+                      ),
                     ),
-                  ),
-                  subtitle: Text(
-                    transaction.contact.accountNumber.toString(),
-                    style: TextStyle(
-                      fontSize: 16.0,
-                    ),
-                  ),
-                ),
-            );
-          },
-          itemCount: transactions.length,
+                  );
+                },
+                itemCount: transactions.length,
+              );
+              break;
+          }
+        return Text('Unknow error');
+        },
       ),
     );
   }
